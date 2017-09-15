a2830fc9b2af4f367252f904f45f3b07c6f21455
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-09-15 21:20:03 +0200

    Add some really nice seeds to dis app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,70 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+game = Game.create!(
+  name: 'Beer game',
+  max_participants: 4
+)
+
+question1 = Question.create!(
+  title: 'What does IPA stand for',
+  game_id: game.id
+)
+
+Option.create!(
+  title: 'Indian pale american',
+  question_id: question1.id,
+  correct: false
+)
+
+Option.create!(
+  title: 'In poor alley',
+  question_id: question1.id,
+  correct: false
+)
+
+Option.create!(
+  title: 'India pale ale',
+  question_id: question1.id,
+  correct: true
+)
+
+Option.create!(
+  title: 'Indian promise armageddon',
+  question_id: question1.id,
+  correct: false
+)
+
+question2 = Question.create!(
+  title: 'What does APA stand for',
+  game_id: game.id
+)
+
+Option.create!(
+  title: 'All pork alfred',
+  question_id: question2.id,
+  correct: false
+)
+
+Option.create!(
+  title: 'American pale ale',
+  question_id: question2.id,
+  correct: true
+)
+
+Option.create!(
+  title: 'American poor alcoholic',
+  question_id: question2.id,
+  correct: false
+)
+
+Option.create!(
+  title: 'Another pedestrian allowed',
+  question_id: question2.id,
+  correct: false
+)
+
+
+
+
