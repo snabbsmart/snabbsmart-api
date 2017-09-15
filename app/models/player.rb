414ccf5a3808881c06746c86f1c43aeee17189b5
Kevin Sjöberg <kev.sjoberg@gmail.com>
2017-09-15 21:33:28 +0200

    Add insane tokens

--- app/models/player.rb
+++ app/models/player.rb
@@ -3,4 +3,12 @@ class Player < ApplicationRecord
   has_many :answers
 
   validates :nickname, uniqueness: { scope: [:game_id] }
+
+  before_save :generate_token
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.base58(32)
+  end
 end
