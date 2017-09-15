684271b3364cf9e2b54f26bbb0218850f2fb580a
Kevin Sjöberg <kev.sjoberg@gmail.com>
2017-09-15 21:28:11 +0200

    Add token generation

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,12 @@
 
 class Game < ApplicationRecord
   validates :token, presence: true
+
+  before_save :generate_token
+
+  private
+
+  def generate_token
+    self.token = (SecureRandom.random_number(9e5) + 1e5).to_i
+  end
 end
