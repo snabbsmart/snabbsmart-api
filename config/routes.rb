bc6598f37ab28b81cdcf922b46a151bde5493d49
Mikael Lisselgård <mikael.lisselgard@standout.se>
2017-09-15 20:43:05 +0200

    Append /start to games start route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get   'games/:token', to: 'games#show'
-  patch 'games/:token', to: 'games#start'
+  patch 'games/:token/start', to: 'games#start'
   post  'games/:token/players', to: 'players#create'
 end
